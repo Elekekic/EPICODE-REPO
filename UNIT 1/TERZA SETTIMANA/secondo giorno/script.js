29e90e88a34f06773a108fbbbc6e808876e6bc5f
Elenaaaak <kekicelena@gmail.com>
2024-02-07 09:41:39 +0100

    commit

--- UNIT 1/TERZA SETTIMANA/secondo giorno/script.js
+++ UNIT 1/TERZA SETTIMANA/secondo giorno/script.js
@@ -8,7 +8,7 @@ console.log("---ESERCIZIO 1 ----")
 
 
 function Stringainsieme(stringa1, stringa2) {
-    console.log((stringa1.substring(0, 2) + stringa2.slice(-3)).toUpperCase());
+  console.log((stringa1.substring(0, 2) + stringa2.slice(-3)).toUpperCase());
 }
 
 Stringainsieme("Panda", "Rosso");
@@ -34,7 +34,7 @@ console.log("---ESERCIZIO 3 ----")
 const numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function numeriPari(pari) {
-    return pari.filter(numero => numero % 2 === 0)
+  return pari.filter(numero => numero % 2 === 0)
 }
 
 const nuoviNumeri = numeriPari(numeri);
@@ -51,11 +51,11 @@ const numeri2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
 function sommaArray(array) {
-    let somma = 0;
-    array.forEach(numero => {
-        somma += numero;
-    });
-    return somma;
+  let somma = 0;
+  array.forEach(numero => {
+    somma += numero;
+  });
+  return somma;
 }
 
 // Esempio d'uso
@@ -73,8 +73,8 @@ const numeri5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
 function sommaNumeri(array) {
-    let somma = array.reduce((val1, val2) => val1 + val2, 0);
-    return somma;
+  let somma = array.reduce((val1, val2) => val1 + val2, 0);
+  return somma;
 }
 
 const Risultato = sommaNumeri(numeri5);
@@ -90,14 +90,16 @@ console.log(Risultato);
 console.log("---ESERCIZIO 6 ----")
 
 const numeri6 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const valore = 7; 
+const valore = 7;
 
 function secondoArray(array, valore) {
-const buh = array.map(function (val6){ 
-    return val6 * valore}); 
-}; 
+  const qualcosa = array.map(function (val6) {
+    return val6 * valore
+  });
+  return qualcosa;
+};
 
-const Risultato6 = secondoArray(numeri6);
+const Risultato6 = secondoArray(numeri6, valore);
 console.log(Risultato6);
 
 
@@ -118,118 +120,118 @@ console.log("---ESERCIZIO 7 ----")
 
 /* Questo array di film verrà usato negli esercizi a seguire. Non modificarlo e scorri oltre per riprendere gli esercizi :) */
 const movies = [
-    {
-        Title: 'The Lord of the Rings: The Fellowship of the Ring',
-        Year: '2001',
-        imdbID: 'tt0120737',
-        Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg',
-    },
-    {
-        Title: 'The Lord of the Rings: The Return of the King',
-        Year: '2003',
-        imdbID: 'tt0167260',
-        Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
-    },
-    {
-        Title: 'The Lord of the Rings: The Two Towers',
-        Year: '2002',
-        imdbID: 'tt0167261',
-        Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
-    },
-    {
-        Title: 'Lord of War',
-        Year: '2005',
-        imdbID: 'tt0399295',
-        Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
-    },
-    {
-        Title: 'Lords of Dogtown',
-        Year: '2005',
-        imdbID: 'tt0355702',
-        Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
-    },
-    {
-        Title: 'The Lord of the Rings',
-        Year: '1978',
-        imdbID: 'tt0077869',
-        Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
-    },
-    {
-        Title: 'Lord of the Flies',
-        Year: '1990',
-        imdbID: 'tt0100054',
-        Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg',
-    },
-    {
-        Title: 'The Lords of Salem',
-        Year: '2012',
-        imdbID: 'tt1731697',
-        Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg',
-    },
-    {
-        Title: 'Greystoke: The Legend of Tarzan, Lord of the Apes',
-        Year: '1984',
-        imdbID: 'tt0087365',
-        Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg',
-    },
-    {
-        Title: 'Lord of the Flies',
-        Year: '1963',
-        imdbID: 'tt0057261',
-        Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg',
-    },
-    {
-        Title: 'The Avengers',
-        Year: '2012',
-        imdbID: 'tt0848228',
-        Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
-    },
-    {
-        Title: 'Avengers: Infinity War',
-        Year: '2018',
-        imdbID: 'tt4154756',
-        Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
-    },
-    {
-        Title: 'Avengers: Age of Ultron',
-        Year: '2015',
-        imdbID: 'tt2395427',
-        Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg',
-    },
-    {
-        Title: 'Avengers: Endgame',
-        Year: '2019',
-        imdbID: 'tt4154796',
-        Type: 'movie',
-        Poster:
-            'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg',
-    },
+  {
+    Title: 'The Lord of the Rings: The Fellowship of the Ring',
+    Year: '2001',
+    imdbID: 'tt0120737',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg',
+  },
+  {
+    Title: 'The Lord of the Rings: The Return of the King',
+    Year: '2003',
+    imdbID: 'tt0167260',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+  },
+  {
+    Title: 'The Lord of the Rings: The Two Towers',
+    Year: '2002',
+    imdbID: 'tt0167261',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Lord of War',
+    Year: '2005',
+    imdbID: 'tt0399295',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Lords of Dogtown',
+    Year: '2005',
+    imdbID: 'tt0355702',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
+  },
+  {
+    Title: 'The Lord of the Rings',
+    Year: '1978',
+    imdbID: 'tt0077869',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Lord of the Flies',
+    Year: '1990',
+    imdbID: 'tt0100054',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg',
+  },
+  {
+    Title: 'The Lords of Salem',
+    Year: '2012',
+    imdbID: 'tt1731697',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Greystoke: The Legend of Tarzan, Lord of the Apes',
+    Year: '1984',
+    imdbID: 'tt0087365',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Lord of the Flies',
+    Year: '1963',
+    imdbID: 'tt0057261',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg',
+  },
+  {
+    Title: 'The Avengers',
+    Year: '2012',
+    imdbID: 'tt0848228',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Avengers: Infinity War',
+    Year: '2018',
+    imdbID: 'tt4154756',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Avengers: Age of Ultron',
+    Year: '2015',
+    imdbID: 'tt2395427',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg',
+  },
+  {
+    Title: 'Avengers: Endgame',
+    Year: '2019',
+    imdbID: 'tt4154796',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg',
+  },
 ]
 
 /* ESERCIZIO 9 (forEach)
