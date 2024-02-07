fc60cf05017fd607da31479ce50648816bdbab23
Elenaaaak <kekicelena@gmail.com>
2024-02-07 23:26:23 +0100

    commit

--- UNIT 1/TERZA SETTIMANA/terzo giorno/assets/js/script.js
+++ UNIT 1/TERZA SETTIMANA/terzo giorno/assets/js/script.js
@@ -1,5 +1,13 @@
 const newTaskInput = document.getElementById("new_task")
 const todoList = document.getElementById("todo_list")
+const button = document.getElementById("button")
+// 
+let array =[];
+
+
+window.addEventListener('load', init);
+
+
 /*
 prima prova : 
  function aggiungiTask() {
@@ -7,7 +15,6 @@ prima prova :
     li.innerHTML = todoList.value;
     todoList.appendChild(newTaskInput);
     } */ 
-
 function aggiungiTask() {
     let li = document.createElement("li");
     li.textContent = newTaskInput.value;
