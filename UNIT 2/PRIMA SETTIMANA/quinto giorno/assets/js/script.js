8b287f9e5702f04a770a0c1cf24ede8e763db71b
Elenaaaak <kekicelena@gmail.com>
2024-02-24 01:49:40 +0100

    commit

--- UNIT 2/PRIMA SETTIMANA/quinto giorno/assets/js/script.js
+++ UNIT 2/PRIMA SETTIMANA/quinto giorno/assets/js/script.js
@@ -1,1 +1,63 @@
-w3.includeHTML();
+const navChange = document.getElementById('nav'); 
+const buttonChange = document.getElementById('buttons'); 
+const lettereM = document.querySelectorAll('#lettere > g'); 
+const M = document.getElementById('M > g'); 
+console.log(M);
+console.log(lettereM.length);
+
+
+/* lettereM.forEach((gElement, i) => {
+    setTimeout(() => {
+        gElement.style.fill = 'transparent';
+    }, i * 400); 
+}); */
+
+/* lettereM.forEach(function(lettera) {
+    setInterval(function() {
+        alternareVisibilita(lettera);
+    }, 200);
+});
+ */
+
+
+
+for (let i = 0; i < lettereM.length; i++ ) {
+
+let numeroRandom = Math.floor(Math.random() * lettereM.length); 
+
+ lettereM[numeroRandom]
+ setTimeout(() => {
+    alternareVisibilita(lettereM[numeroRandom])
+}, i * 10);
+
+}
+
+window.addEventListener('scroll', function() {
+    const scrollVerticale = window.scrollY; 
+
+    const scrollIntercettazione = 439; 
+
+
+    if (scrollVerticale > scrollIntercettazione) {
+     navChange.style.backgroundColor = 'white';
+     buttonChange.style.backgroundColor ="#1a8917"
+    } else {
+       navChange.style.backgroundColor = '';  
+       buttonChange.style.backgroundColor = ''; 
+    }
+}); 
+
+
+function alternareVisibilita(lettera) {
+    if (lettera.style.fill === 'black') {
+        lettera.style.fill = 'transparent';
+    } else {
+        lettera.style.fill = 'black';
+    }
+}
+
+
+
+
+
+
