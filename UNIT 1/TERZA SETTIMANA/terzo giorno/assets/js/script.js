5c06cc6fbea6dca4d662a1448e4da17819307b0b
Elenaaaak <kekicelena@gmail.com>
2024-02-25 12:48:53 +0100

    commit

--- UNIT 1/TERZA SETTIMANA/terzo giorno/assets/js/script.js
+++ UNIT 1/TERZA SETTIMANA/terzo giorno/assets/js/script.js
@@ -14,7 +14,7 @@ const init = () => {
     button.addEventListener("click", aggiungiTask);
 
     //Ho messo questo giusto per non andare sempre a cliccare il button 
-    newTaskInput.addEventListener("keypress", function(event) {
+    newTaskInput.addEventListener("keypress", function (event) {
         // Verifica se il tasto premuto è il tasto "Invio" 
         if (event.key === "Enter") {
             aggiungiTask();
@@ -48,20 +48,29 @@ const aggiungiTask = () => {
                 </button>
             </div>`;
 
+        // appende la lista nella ul
         todoList.appendChild(li);
+        // resetta il valore dell'input a vuoto
         newTaskInput.value = "";
 
-        // richiamo la funzione per far si che ad ogni click del button con il cestino, si eliminerà dalla ul__
+        // richiamo la funzione per far si che al click del cestino, si eliminerà dalla ul 
         taskCompletata();
         // Aggiorna il localStorage con la lista delle task
         aggiornaLocalStorage();
     }
 }
 
+
+// funzione che mi permette di eliminare una task dalla lista
 const taskCompletata = () => {
+
+    //creo variabile che mi prende in una Nodelist tutti gli elementi con la classe "delete"
     let buttons = document.querySelectorAll(".delete");
+    // per ogni button presente nella ul, se essa viene cliccata si eliminerà
     buttons.forEach(button => {
         button.addEventListener("click", function () {
+            // "this" = button , "parentNode" = elemento padre del pulsante, cioè todoList (ul) 
+            // "remove" = rimuove tutto il contenuto all'interno del div "task"
             this.parentNode.remove();
             // Aggiorna il localStorage dopo l'eliminazione di una task
             aggiornaLocalStorage();
@@ -70,10 +79,16 @@ const taskCompletata = () => {
 }
 
 
-
+// funzione che mi permette si salvare la lista attualmente presente nella ul nel LocalStorage
 const aggiornaLocalStorage = () => {
-    localStorage.setItem("todoList", todoList.innerHTML); // Salva la lista delle task nel localStorage
+    localStorage.setItem("todoList", todoList.innerHTML); // memorizza i dati con la chiave "todoList" e il contenuto HTML dell'elemento todoList
 }
+
+/* funzione che recupera i dati salvati precedentemente sopra. 
+ Se nel localStorage è presente una lista delle task salvata precedentemente, 
+ questa funzione imposta il contenuto HTML dell'elemento todoList con il contenuto
+ recuperato dal localStorage, ripristinando così la lista delle task sulla pagina. 
+ Poi, chiama anche la funzione TaskCompletata per aggiungere gli eventi al button con il cestino */
 
 const ripristinaLista = () => {
     const lista = localStorage.getItem("todoList"); // Ottieni la lista delle task dal localStorage
