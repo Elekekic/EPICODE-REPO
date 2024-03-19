4e5a8b9b4baa43a383afa98a6f826824189d9f56
Elenaaaak <kekicelena@gmail.com>
2024-03-19 15:10:24 +0100

    commit

---
+++ UNIT 3/secondo giorno/secondo-giorno/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'secondo-giorno';
+  content = 'Questa è la prima lezione di Angular (sottotitolo)';
+}
