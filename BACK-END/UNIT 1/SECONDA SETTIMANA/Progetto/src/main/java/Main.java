0f9722c97cd344ce4a4079481ed434437ac9b1dc
Elenaaaak <kekicelena@gmail.com>
2024-04-27 11:50:10 +0200

    commit

--- BACK-END/UNIT 1/SECONDA SETTIMANA/Progetto/src/main/java/Main.java
+++ BACK-END/UNIT 1/SECONDA SETTIMANA/Progetto/src/main/java/Main.java
@@ -3,83 +3,68 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Object> oggetti = new ArrayList<>();
+        Map<Integer, Object> archivio = new HashMap<>();
 
-        oggetti.add(new Libri(389012312, "Java Programming", 2022, 400, "John Doe", "Programming", Periodicita.SETTIMANALE));
-        oggetti.add(new Libri(839291232, "Data Structures and Algorithms", 2020, 400, "Jane Doe", "Computer Science", Periodicita.SEMESTRALE));
-        oggetti.add(new Libri(783238128, "Introduction to Java", 2019, 300, "John Smith", "Programming", Periodicita.SETTIMANALE));
-        oggetti.add(new Libri(293847104, "To Kill a Mockingbird", 1960, 281, "Harper Lee", "Fiction", Periodicita.MENSILE));
-        oggetti.add(new Libri(938272112, "The Great Gatsby", 1925, 180, "F. Scott Fitzgerald", "Fiction", Periodicita.MENSILE));
+        // AGGIUNGO IO DEI LIBRI
+        archivio.put(389012312, new Libri(389012312, "Java Programming", 2022, 400, "John Doe", "Programming", Periodicita.SETTIMANALE));
+        archivio.put(839291232, new Libri(839291232, "Data Structures and Algorithms", 2020, 400, "Jane Doe", "Computer Science", Periodicita.SEMESTRALE));
+        archivio.put(783238128, new Libri(783238128, "Introduction to Java", 2019, 300, "John Smith", "Programming", Periodicita.SETTIMANALE));
+        archivio.put(293847104, new Libri(293847104, "To Kill a Mockingbird", 1960, 281, "Harper Lee", "Fiction", Periodicita.MENSILE));
+        archivio.put(938272112, new Libri(938272112, "The Great Gatsby", 1925, 180, "F. Scott Fitzgerald", "Fiction", Periodicita.MENSILE));
 
+        // AGGIUNGO IO DELLE RIVISTE
+        archivio.put(987654321, new Riviste(987654321, "National Geographic", 2024, 100, "National Geographic Society", "Science and Nature", Periodicita.MENSILE));
+        archivio.put(472847772, new Riviste(472847772, "Time", 2024, 80, "Time Magazine", "Current Affairs", Periodicita.SETTIMANALE));
+        archivio.put(647283024, new Riviste(647283024, "Scientific American", 2023, 70, "Scientific American", "Science", Periodicita.MENSILE));
+        archivio.put(847729102, new Riviste(847729102, "National Geographic Kids", 2024, 90, "National Geographic Society", "Science and Nature", Periodicita.MENSILE));
+        archivio.put(112038281, new Riviste(112038281, "Sports Illustrated", 2023, 60, "Sports Illustrated", "Sports", Periodicita.SEMESTRALE));
 
-        oggetti.add(new Riviste(987654321, "National Geographic", 2024, 100, "National Geographic Society", "Science and Nature", Periodicita.MENSILE));
-        oggetti.add(new Riviste(472847772, "Time", 2024, 80, "Time Magazine", "Current Affairs", Periodicita.SETTIMANALE));
-        oggetti.add(new Riviste(647283024, "Scientific American", 2023, 70, "Scientific American", "Science", Periodicita.MENSILE));
-        oggetti.add(new Riviste(847729102, "National Geographic Kids", 2024, 90, "National Geographic Society", "Science and Nature", Periodicita.MENSILE));
-        oggetti.add(new Riviste(112038281, "Sports Illustrated", 2023, 60, "Sports Illustrated", "Sports", Periodicita.SEMESTRALE));
 
         Scanner scanner = new Scanner(System.in);
-//        System.out.println("Current items in the catalogue:");
-//        for (Object obj : oggetti) {
-//            System.out.println(obj);
-//        }
-//        System.out.println("--------------");
-//        // AGGIUNTA BOOK
-//        System.out.println("Welcome to your catalogue!");
-//        System.out.println("Add a book!");
-//        Libri book = addBook();
-//        oggetti.add(book);
-//
-//        System.out.println();
-//        // AGGUNTA MAGAZINE
-//        System.out.println("Now, add a magazine!");
-//        Riviste magazine = addMagazine();
-//        oggetti.add(magazine);
-//
-//        System.out.println("--------------");
-//        System.out.println("Added items in the catalogue:");
-//        for (Object obj : oggetti) {
-//            System.out.println(obj);
-//        }
-//        System.out.println("--------------");
-//
-//        // RIMOZIONE ITEM TRAMITE ISBN
-//        System.out.println("Inserisci l'ISBN del prodotto che vuoi ELIMINARE: ");
-//        int ISBN = scanner.nextInt();
-//        removeByISBN(ISBN, oggetti);
-//        System.out.println("--------------");
-//
-//
-//        //RICERCA ITEMS TRAMITE ISBN
-//        System.out.println("Inserisci l'ISBN del prodotto che vuoi TROVARE: ");
-//        int ISBN2 = scanner.nextInt();
-//        searchByISBN(ISBN2, oggetti);
-//
-//        System.out.println("--------------");
-//
-//        //RICERCA ITEMS TRAMITE ANNO DI PUBBLICAZIONE
-//        System.out.println("Inserisci l'anno di pubblicazione dei prodotti che vuoi trovare: ");
-//        int annoPubblicazione = scanner.nextInt();
-//        searchByAnnoPubblicazione(annoPubblicazione, oggetti);
-//
-//        System.out.println("--------------");
-//
-//        //RICERCA ITEMS TRAMITE AUTORE
-//        System.out.println("Inserisci l'anno di pubblicazione dei prodotti che vuoi trovare: ");
-//        String autore = scanner.nextLine();
-//        searchByAutore(autore, oggetti);
+        System.out.println("Current items in the catalogue:");
+        archivio.forEach((isbn, item) -> System.out.println("ISBN: " + isbn + ", Item: " + item));
+        System.out.println("--------------");
 
-        String oggettiInString = oggetti.stream()
+        // AGGIUNTA BOOK
+        System.out.println("Welcome to your catalogue!");
+        System.out.println("Add a book!");
+        Libri book = addBook();
+        archivio.put(book.getCodiceISBN(), book);
+
+        // AGGUNTA MAGAZINE
+        System.out.println("Now, add a magazine!");
+        Riviste magazine = addMagazine();
+        archivio.put(magazine.getCodiceISBN(), magazine);
+
+        // RIMOZIONE ITEM TRAMITE ISBN
+        System.out.println("Enter the ISBN of the item you want to REMOVE: ");
+        int ISBN = scanner.nextInt();
+        removeByISBN(ISBN, archivio);
+
+        // RICERCA ITEMS TRAMITE ISBN
+        System.out.println("Enter the ISBN of the item you want to FIND: ");
+        int ISBN2 = scanner.nextInt();
+        searchByISBN(ISBN2, archivio);
+
+        // RICERCA ITEMS TRAMITE ANNO DI PUBBLICAZIONE
+        System.out.println("Enter the year of publication of the items you want to find: ");
+        int annoPubblicazione = scanner.nextInt();
+        searchByAnnoPubblicazione(annoPubblicazione, archivio);
+
+        // RICERCA ITEMS TRAMITE AUTORE
+        scanner.nextLine(); // Consuma il carattere newline rimanente
+        System.out.println("Enter the author of the items you want to find: ");
+        String autore = scanner.nextLine();
+        searchByAutore(autore, archivio);
+
+        String oggettiInString = archivio.values().stream()
                 .map(Object::toString)
                 .collect(Collectors.joining("#"));
 
@@ -167,59 +152,50 @@ public class Main {
         return new Riviste(isbn, title, year, pages, author, genre, periodicita);
     }
 
-
-    private static void removeByISBN(int ISBN, List<Object> oggetti) {
-        oggetti.removeIf(obj -> {
-            if (obj instanceof Libri) {
-                return ((Libri) obj).getCodiceISBN() == ISBN;
-            } else if (obj instanceof Riviste) {
-                return ((Riviste) obj).getCodiceISBN() == ISBN;
-            }
-            return false;
-        });
-
-        System.out.println("Item with ISBN " + ISBN + " removed.");
-        oggetti.forEach(System.out::println);
+    private static void removeByISBN(int ISBN, Map<Integer, Object> oggettiMap) {
+        if (oggettiMap.containsKey(ISBN)) {
+            oggettiMap.remove(ISBN);
+            System.out.println("Item with ISBN " + ISBN + " removed.");
+        } else {
+            System.out.println("Item with ISBN " + ISBN + " not found.");
+        }
     }
 
-    private static void searchByISBN(int ISBN, List<Object> oggetti) {
-        List<Object> filteredList = oggetti.stream().filter(obj -> {
-                    if (obj instanceof Libri) {
-                        return ((Libri) obj).getCodiceISBN() == ISBN;
-                    } else if (obj instanceof Riviste) {
-                        return ((Riviste) obj).getCodiceISBN() == ISBN;
-                    }
-                    return false;
-                }).toList();
-
-        filteredList.forEach(System.out::println);
+    private static void searchByISBN(int ISBN, Map<Integer, Object> oggettiMap) {
+        Object item = oggettiMap.get(ISBN);
+        if (item != null) {
+            System.out.println("Item found: " + item);
+        } else {
+            System.out.println("Item with ISBN " + ISBN + " not found.");
+        }
     }
 
-    private static void searchByAnnoPubblicazione(int anno, List<Object> oggetti) {
-        List<Object> filteredList = oggetti.stream().filter(obj -> {
+    private static void searchByAnnoPubblicazione(int anno, Map<Integer, Object> oggettiMap) {
+        oggettiMap.values().stream()
+                .filter(obj -> {
                     if (obj instanceof Libri) {
                         return ((Libri) obj).getAnnoPubblicazione() == anno;
                     } else if (obj instanceof Riviste) {
                         return ((Riviste) obj).getAnnoPubblicazione() == anno;
                     }
                     return false;
-                }).toList();
-
-        filteredList.forEach(System.out::println);
+                })
+                .forEach(System.out::println);
     }
 
-    private static void searchByAutore(String autore, List<Object> oggetti) {
-        List<Object> filteredList = oggetti.stream().filter(obj -> {
-            if (obj instanceof Libri) {
-                return ((Libri) obj).getAutore() == autore;
-            } else if (obj instanceof Riviste) {
-                return ((Riviste) obj).getAutore() == autore;
-            }
-            return false;
-        }).toList();
-
-        filteredList.forEach(System.out::println);
+    private static void searchByAutore(String autore, Map<Integer, Object> oggettiMap) {
+        oggettiMap.values().stream()
+                .filter(obj -> {
+                    if (obj instanceof Libri) {
+                        return ((Libri) obj).getAutore().equals(autore);
+                    } else if (obj instanceof Riviste) {
+                        return ((Riviste) obj).getAutore().equals(autore);
+                    }
+                    return false;
+                })
+                .forEach(System.out::println);
     }
+}
 
 
 
