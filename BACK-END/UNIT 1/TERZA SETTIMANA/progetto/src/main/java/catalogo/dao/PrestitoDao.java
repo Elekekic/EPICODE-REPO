f8370a6646fec991390c506afc0f5449f4f0eec0
Elenaaaak <kekicelena@gmail.com>
2024-05-04 18:25:01 +0200

    commit

--- BACK-END/UNIT 1/TERZA SETTIMANA/progetto/src/main/java/catalogo/dao/PrestitoDao.java
+++ BACK-END/UNIT 1/TERZA SETTIMANA/progetto/src/main/java/catalogo/dao/PrestitoDao.java
@@ -43,7 +43,7 @@ public class PrestitoDao {
     //7. Ricerca degli elementi ATTUALMENTE IN PRESTITO dato un numero tessera utente
     public List<Prestito> getByNumeroTesseraUtente(int numeroTessera) {
         Query query = em.createQuery("SELECT p FROM Prestito p WHERE p.utente.numeroTessera = :numeroTessera AND p.dataRestituzioneEffettiva IS NULL");
-        query.setParameter("numeroTessera", numeroTessera); // Corretto il nome del parametro
+        query.setParameter("numeroTessera", numeroTessera);
         return query.getResultList();
     }
 
