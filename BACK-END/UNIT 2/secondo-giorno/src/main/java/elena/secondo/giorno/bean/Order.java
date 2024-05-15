df968e9b67506b96d37daa7596c5bddd7b494603
Elenaaaak <kekicelena@gmail.com>
2024-05-15 13:51:37 +0200

    commit

--- BACK-END/UNIT 2/secondo-giorno/src/main/java/elena/secondo/giorno/bean/Order.java
+++ BACK-END/UNIT 2/secondo-giorno/src/main/java/elena/secondo/giorno/bean/Order.java
@@ -3,7 +3,6 @@ package elena.secondo.giorno.bean;
 import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalTime;
