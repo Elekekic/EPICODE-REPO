f48a5262a0ef996259b4e0941f413a53ad782106
Elenaaaak <kekicelena@gmail.com>
2024-05-21 15:55:17 +0200

    commit

---
+++ BACK-END/UNIT 3/secondo-giorno/src/main/java/elena/secondogiorno/SecondoGiornoApplication.java
@@ -0,0 +1,13 @@
+package elena.secondogiorno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondoGiornoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondoGiornoApplication.class, args);
+	}
+
+}
