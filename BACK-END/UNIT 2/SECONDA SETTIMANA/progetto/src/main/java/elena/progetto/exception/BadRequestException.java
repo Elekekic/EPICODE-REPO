47a9bef171ce8bdc2ec1a33c153744b214ae94e7
Elenaaaak <kekicelena@gmail.com>
2024-05-24 18:30:04 +0200

    commit

--- BACK-END/UNIT 2/SECONDA SETTIMANA/progetto/src/main/java/elena/progetto/exception/BadRequestException.java
+++
@@ -1,8 +0,0 @@
-package elena.progetto.exception;
-
-public class BadRequestException extends RuntimeException{
-
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
--- BACK-END/UNIT 2/SECONDA SETTIMANA/progetto/src/main/java/elena/progetto/repository/ComputerRepository.java
+++
@@ -1,8 +0,0 @@
-package elena.progetto.repository;
-
-import elena.progetto.model.Computer;
-import elena.progetto.model.Dispositivo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComputerRepository extends JpaRepository<Computer, Integer> {
-}
