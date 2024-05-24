e8567332c17298605cf8e94e9894375af7d8af5f
Elenaaaak <kekicelena@gmail.com>
2024-05-24 18:28:03 +0200

    commit

--- BACK-END/UNIT 2/SECONDA SETTIMANA/progetto/src/main/java/elena/progetto/repository/DipendenteRepository.java
+++
@@ -1,8 +0,0 @@
-package elena.progetto.repository;
-
-import elena.progetto.model.Dipendente;
-import elena.progetto.model.Dispositivo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DipendenteRepository extends JpaRepository<Dipendente, Integer> {
-}
--- BACK-END/UNIT 2/SECONDA SETTIMANA/progetto/src/main/java/elena/progetto/repository/DispositivoRepository.java
+++
@@ -1,7 +0,0 @@
-package elena.progetto.repository;
-
-import elena.progetto.model.Dispositivo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DispositivoRepository extends JpaRepository<Dispositivo, Integer> {
-}
